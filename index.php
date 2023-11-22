253b024d409da62e199a4e11a8cf0c364a764208
IlyaHar <harcenkoila278@gmail.com>
2023-11-22 17:31:18 +0200

    add class User

--- index.php
+++ index.php
@@ -1,39 +1,68 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
-trait Trait1
+class User
 {
-   public function test() {
-        return 1;
+    private string $name;
+    private int $age;
+    private string $email;
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+    public function setAge(int $age): void
+    {
+        $this->age = $age;
+    }
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function __call(string $method, array $arguments)
+    {
+        if (method_exists($this, $method)) {
+            return $this->$method( ... $arguments);
+        } else {
+            throw new Exception('Такого методу не існує!');
+        }
+    }
+    public function getAll() {
+        return [
+            'name' => $this->name,
+            'age' => $this->age,
+            'email' => $this->email
+        ];
     }
 }
 
-trait Trait2
-{
-   public function test() {
-        return 2;
-    }
+
+// Для виводу даних User
+try {
+    $user = new User();
+    $user->setName('Test');
+    $user->setAge(17);
+    $user->setEmail('test@gmail.com');
+
+
+    dd($user->getAll());
+
+} catch (Exception $e) {
+    echo 'Помилка:' . "\n" . $e->getMessage();
 }
 
-trait Trait3
-{
-    public function test() {
-        return 3;
-    }
-}
 
- class Test
-{
-    use Trait1, Trait2, Trait3 {
-        Trait1::test insteadof Trait2, Trait3;
-        Trait2::test as test2;
-        Trait3::test as test3;
-    }
-    function getSum() {
-        return $this->test() + $this->test2() + $this->test3();
-    }
- }
+// Для виводу помилки
+/*try {
+    $user = new User();
+    $user->setName('Test');
+    $user->setAge('17');
+    $user->setError('Error');
 
- $test = new Test;
-dd($test->test(), $test->test2(), $test->test3(), $test->getSum());
+
+    dd($user->getAll());
+
+} catch (Exception $e) {
+    echo 'Помилка:' . "\n" . $e->getMessage();
+}*/
 
