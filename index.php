8be996f69f6d5e2ba0c8c804592c0f31d2dc4cc2
IlyaHar <harcenkoila278@gmail.com>
2023-11-22 14:09:58 +0200

    add traits

--- index.php
+++ index.php
@@ -1,106 +1,39 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
-class ValueObject
+trait Trait1
 {
-    private $red;
-    private $green;
-    private $blue;
-
-    public function __construct($red, $green, $blue)
-    {
-        $this->setRed($red);
-        $this->setGreen($green);
-        $this->setBlue($blue);
-    }
-
-    public function getRed()
-    {
-        return $this->red;
-    }
-
-    public function getGreen()
-    {
-        return $this->green;
-    }
-
-    public function getBlue()
-    {
-        return $this->blue;
-    }
-
-    public function setRed($red): void
-    {
-
-        if ($this->ValidColor($red)) {
-            $this->red = $red;
-        } else {
-            echo 'Це не валідний колір!';
-        }
-
-    }
-
-    public function setGreen($green): void
-    {
-        if ($this->ValidColor($green)) {
-            $this->green = $green;
-        } else {
-            echo 'Це не валідний колір!';
-        }
-    }
-
-    public function setBlue($blue): void
-    {
-        if ($this->ValidColor($blue)) {
-            $this->blue = $blue;
-        } else {
-            echo 'Це не валідний колір!';
-        }
-
-    }
-
-    private function ValidColor($value)
-    {
-        return $value >= 0 && $value <= 255;
-    }
-
-    public function equals($color)
-    {
-        if ($color instanceof ValueObject) {
-            return
-                $this->red === $color->getRed() &&
-                $this->green === $color->getGreen() &&
-                $this->blue === $color->getBlue();
-        } else {
-            return false;
-        }
-    }
-
-    public static function random()
-    {
-        $red = rand(0, 255);
-        $green = rand(0, 255);
-        $blue = rand(0, 255);
-
-        return new ValueObject($red, $green, $blue);
-    }
-
-    public function mix($color)
-    {
-        $mixedRed = round(($this->red + $color->getRed()) / 2);
-        $mixedGreen = round(($this->green + $color->getGreen()) / 2);
-        $mixedBlue = round(($this->blue + $color->getBlue()) / 2);
-
-        if ($this->ValidColor($mixedRed) && $this->ValidColor($mixedGreen) && $this->ValidColor($mixedBlue)) {
-            return new ValueObject($mixedRed, $mixedGreen, $mixedBlue);
-        }
+   public function test() {
+        return 1;
     }
 }
 
-$color = new ValueObject(250, 250, 250);
-$mixedColor = $color->mix(new ValueObject(100, 100, 100));
-echo $mixedColor->getRed() . "\n";
-echo $mixedColor->getGreen() . "\n";
-echo $mixedColor->getBlue() . "\n";
+trait Trait2
+{
+   public function test() {
+        return 2;
+    }
+}
 
+trait Trait3
+{
+    public function test() {
+        return 3;
+    }
+}
+
+ class Test
+{
+    use Trait1, Trait2, Trait3 {
+        Trait1::test insteadof Trait2, Trait3;
+        Trait2::test as test2;
+        Trait3::test as test3;
+    }
+    function getSum() {
+        return $this->test() + $this->test2() + $this->test3();
+    }
+ }
+
+ $test = new Test;
+dd($test->test(), $test->test2(), $test->test3(), $test->getSum());
 
