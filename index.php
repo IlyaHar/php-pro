f63806cff72ab8fc92e350598c4938521a82297f
IlyaHar <harcenkoila278@gmail.com>
2023-11-30 15:42:30 +0200

    add Taxi System Factory Method

--- index.php
+++ index.php
@@ -1,27 +1,87 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
-interface Database {
-    public function getData();
+interface TypeTaxi {
+    public function getModel(): string;
+    public  function getPrice(): int;
 }
-class Mysql implements Database
-{
-    public function getData()
+
+class Economy implements TypeTaxi {
+
+    public function getModel(): string
     {
-        return 'some data from database';
+        return 'Renault Logan';
+    }
+
+    public function getPrice(): int
+    {
+        return 5;
     }
 }
 
-class Controller implements Database
-{
-    private $adapter;
-    public function __construct(Database $adapter)
+class Standard implements TypeTaxi {
+
+    public function getModel(): string
     {
-        $this->adapter = $adapter;
+        return 'Toyota Camry';
     }
-    public function getData()
+
+    public function getPrice(): int
     {
-        return $this->adapter->getData();
+        return 15;
     }
 }
 
+class Luxury implements TypeTaxi {
+
+    public function getModel(): string
+    {
+        return 'Mercedes-Benz Maybach';
+    }
+
+    public function getPrice(): int
+    {
+        return 50;
+    }
+}
+
+abstract class Logistic {
+    abstract function getTypeTaxi(): TypeTaxi;
+    public function taxi(): TypeTaxi {
+        return $this->getTypeTaxi();
+    }
+}
+
+class EconomyTaxi extends Logistic {
+
+    function getTypeTaxi(): TypeTaxi
+    {
+        return new Economy();
+    }
+}
+
+class StandardTaxi extends Logistic {
+
+    function getTypeTaxi(): TypeTaxi
+    {
+        return new Standard();
+    }
+}
+
+class LuxuryTaxi extends Logistic {
+
+    function getTypeTaxi(): TypeTaxi
+    {
+        return new Luxury();
+    }
+}
+
+function clientCode(Logistic $logistic) {
+    $taxi = $logistic->taxi();
+    echo 'Model: ' . $taxi->getModel() . "\n";
+    echo 'Price: $' . $taxi->getPrice() . "\n";
+}
+
+clientCode(new EconomyTaxi());
+clientCode(new StandardTaxi());
+clientCode(new LuxuryTaxi());
